8d87bf2f2534e6f6f689b8036353f051cdd3698c
siyua <siyuanliu3@gmail.com>
2019-03-31 19:50:37 -0600

    added recycler view added top bars to some pages

--- app/src/main/java/com/technovation/mytutors/HomepageFragment.java
+++ app/src/main/java/com/technovation/mytutors/HomepageFragment.java
@@ -3,11 +3,17 @@ package com.technovation.mytutors;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -16,6 +22,11 @@ import android.widget.ImageButton;
 public class HomepageFragment extends Fragment {
 
     private Button profileCard;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private HomepageRecyclerAdaper recyclerAdaper;
+    private List<String> list;
 
 
     public HomepageFragment() {
@@ -29,7 +40,7 @@ public class HomepageFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_homepage, container, false);
 
-
+        /*
         // changing fragment to viewing profile if profile card (on homepage) is clicked
         profileCard = view.findViewById(R.id.profileCardButton);
         profileCard.setOnClickListener(new View.OnClickListener() {
@@ -37,7 +48,17 @@ public class HomepageFragment extends Fragment {
             public void onClick(View v) {
                 MainActivity.fragmentManager.beginTransaction().replace(R.id.FragmentContainer,new ViewProfileFragment(), null).addToBackStack(null).commit();
             }
-        });
+        });\
+        */
+
+        list = Arrays.asList(getResources().getStringArray(R.array.users));
+        recyclerView = view.findViewById(R.id.recycler_view);
+        layoutManager = new LinearLayoutManager(view.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerAdaper = new HomepageRecyclerAdaper(list);
+        recyclerView.setAdapter(recyclerAdaper);
+
+        recyclerView.setHasFixedSize(true);
 
 
 
---
+++ app/src/main/java/com/technovation/mytutors/HomepageRecyclerAdaper.java
@@ -0,0 +1,62 @@
+package com.technovation.mytutors;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class HomepageRecyclerAdaper extends RecyclerView.Adapter<HomepageRecyclerAdaper.myViewHolder> {
+
+    private List<String>list;
+
+    public HomepageRecyclerAdaper(List<String>list){
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        RelativeLayout profileView = (RelativeLayout) LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.profile_card_layout, viewGroup,false);
+
+        myViewHolder viewHolder = new myViewHolder(profileView);
+
+        return viewHolder;
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myViewHolder viewHolder, int i) {
+        String profile = list.get(i);
+        String name = profile.split(",")[0];
+        String pic = profile.split(",")[1];
+
+        viewHolder.textView.setText(name);
+
+    }
+
+    public static class myViewHolder extends RecyclerView.ViewHolder {
+
+        private RelativeLayout profileView;
+        private ImageView imageView;
+        private TextView textView;
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.profileView = (RelativeLayout)itemView;
+            this.textView = this.profileView.findViewById(R.id.profileName);
+            this.imageView = this.profileView.findViewById(R.id.profilePic);
+        }
+    }
+
+}
